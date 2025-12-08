970ff58b4ff6c3e60efeca71a147967b313e8398
Arun Sankar <imarunsankarks@gmail.com>
2025-12-08 23:07:56 +0530

    largestOdd number

--- string.js
+++ string.js
@@ -3,4 +3,15 @@ const palindrome = (str) => {
     return str.trim().split(" ").reverse().join(" ")
 }
 
-console.log(palindrome("welcome to the jungle"))
+// console.log(palindrome("welcome to the jungle"))
+
+const largestOdd = (num) => {
+    let numbr = parseInt(num)
+    while (numbr % 2 === 0) {
+        numbr = Math.trunc(numbr / 10)
+    }
+    return `${numbr}`
+}
+
+console.log(largestOdd("00214638922"));
+
