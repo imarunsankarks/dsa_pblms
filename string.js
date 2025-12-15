729d042b3650354079e77b23df129a67d420712c
Arun Sankar <imarunsankarks@gmail.com>
2025-12-15 23:07:29 +0530

    Isomorphic strings

--- string.js
+++ string.js
@@ -28,4 +28,29 @@ const commonPrefix = (arr) => {
 
 // console.log(commonPrefix(['profile', 'profix', 'proplan', 'pro']));
 
+const isomorphic = (s, t) => {
+    const s1 = new Set(s)
+    const t1 = new Set(t)
+    let m1 = new Map()
+    if (s1.size == t1.size && s.length == t.length) {
+        const sArr = [...s1];
+        const tArr = [...t1];
+
+        for (let i = 0; i < sArr.length; i++) {
+            m1.set(sArr[i], tArr[i]);
+        }
+        for (let j = 0; j < s.length; j++) {
+            if (m1.get(s[j]) != t[j]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isomorphic("bbbaaaba", "aaabbbba"));
+
+
 
