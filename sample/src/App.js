95af6e8501dee5e2435f81889ebe5dab2fb5f08f
arunsankar2 <arun.sankar@grasko.com>
2026-01-27 12:33:55 +0530

    Context added

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,9 +1,12 @@
 import Table from "./comps/table";
 import Search from "./comps/search"
+import { useTheme } from "./context/theme";
 
 export default function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="App">
+    <div className={theme}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Table />
       <Search />
     </div>
--- sample/src/index.js
+++ sample/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './context/theme';
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
